2b1910118325fcbee9b1bc354edf3d6ba3d7c93a
Sirojiddin <sirojboy@inbox.ru>
2024-03-14 10:48:37 +0500

    done hw

--- Handling Errors in Programming/sem1/hw1.java
+++ Handling Errors in Programming/sem1/hw1.java
@@ -1,11 +1,12 @@
 /**
-Реализуйте 3 метода, чтобы в каждом из них получить разные исключения.
-
-Метод `arrayOutOfBoundsException` - Ошибка, связанная с выходом за пределы массива
-Метод `divisionByZero` - Деление на 0
-Метод `numberFormatException` - Ошибка преобразования строки в число
-
-Важно: они не должны принимать никаких аргументов
+ * Реализуйте 3 метода, чтобы в каждом из них получить разные исключения.
+ * 
+ * Метод `arrayOutOfBoundsException` - Ошибка, связанная с выходом за пределы
+ * массива
+ * Метод `divisionByZero` - Деление на 0
+ * Метод `numberFormatException` - Ошибка преобразования строки в число
+ * 
+ * Важно: они не должны принимать никаких аргументов
  */
 public class hw1 {
     public static void main(String[] args) {
@@ -27,21 +28,22 @@ public class hw1 {
             System.out.println("Ошибка преобразования строки в число");
         }
     }
-    
+
     class Answer {
         public static void arrayOutOfBoundsException() {
             // Напишите свое решение ниже
-          
-    }
-    
+            int[] array = new int[1];
+            int number = array[2];
+        }
+
         public static void divisionByZero() {
             // Напишите свое решение ниже
-          
-    }
-    
+            int result = 10 / 0;
+        }
+
         public static void numberFormatException() {
             // Напишите свое решение ниже
-           
-     }
+            int number = Integer.parseInt("qwe");
+        }
     }
 }
--- Handling Errors in Programming/sem1/hw3.java
+++ Handling Errors in Programming/sem1/hw3.java
@@ -1,9 +1,72 @@
 /*
- * 
+Реализуйте метод divArrays, принимающий в качестве аргументов два целочисленных массива a и b,
+и возвращающий новый массив с, каждый элемент которого равен частному элементов двух входящих 
+массивов в той же ячейке.
+
+Если длины массивов не равны - верните нулевой массив длины 1.
+
+Важно: При выполнении метода единственное исключение, которое пользователь может увидеть - RuntimeException, т.е. ваше.
+
+Напишите свой код в методе divArrays класса Answer. Метод divArrays принимает на вход два параметра:
+
+int[] a - первый массив
+int[] b - второй массив
+Пример
+a = new int[]{12, 8, 16};
+b = new int[]{4, 2, 4};
+
+Вывод: [3, 4, 4]
+
+a = new int[]{12, 8, 16, 25};
+b = new int[]{4, 2, 4};
+
+Вывод: [0]
  */
+
+import java.util.Arrays;
 
 public class hw3 {
     public static void main(String[] args) {
-        
-    }    
+        int[] a = {};
+        int[] b = {};
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = new int[] { 12, 8, 16 };
+            b = new int[] { 4, 2, 4 };
+        } else {
+            a = Arrays.stream(args[0].split(", ")).mapToInt(Integer::parseInt).toArray();
+            b = Arrays.stream(args[1].split(", ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+        Answer ans = new Answer();
+        String itresume_res = Arrays.toString(ans.divArrays(a, b));
+        System.out.println(itresume_res);
+    }
+}
+
+class Answer {
+    public int[] divArrays(int[] a, int[] b) {
+        // Введите свое решение ниже
+        if (a.length != b.length) {
+            return new int[] { 0 };
+        }
+
+        // Создание результирующего массива
+        int[] c = new int[a.length];
+
+        // Вычисление разности для каждого элемента
+        for (int i = 0; i < a.length; i++) {
+            try {
+                if (b[i] == 0) {
+                    throw new RuntimeException("Division by zero at index" + i);
+                }
+                c[i] = a[i] / b[i];
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                return new int[] { 0 };
+            }
+        }
+        return c;
+    }
 }
