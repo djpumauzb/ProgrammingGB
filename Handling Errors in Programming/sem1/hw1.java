24f94f32226d529bf66cda897065d9503f747df1
Sirojiddin <sirojboy@inbox.ru>
2024-03-14 06:59:30 +0500

    add sem1 hws

---
+++ Handling Errors in Programming/sem1/hw1.java
@@ -0,0 +1,47 @@
+/**
+Реализуйте 3 метода, чтобы в каждом из них получить разные исключения.
+
+Метод `arrayOutOfBoundsException` - Ошибка, связанная с выходом за пределы массива
+Метод `divisionByZero` - Деление на 0
+Метод `numberFormatException` - Ошибка преобразования строки в число
+
+Важно: они не должны принимать никаких аргументов
+ */
+public class hw1 {
+    public static void main(String[] args) {
+        try {
+            Answer.arrayOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Выход за пределы массива");
+        }
+
+        try {
+            Answer.divisionByZero();
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль");
+        }
+
+        try {
+            Answer.numberFormatException();
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка преобразования строки в число");
+        }
+    }
+    
+    class Answer {
+        public static void arrayOutOfBoundsException() {
+            // Напишите свое решение ниже
+          
+    }
+    
+        public static void divisionByZero() {
+            // Напишите свое решение ниже
+          
+    }
+    
+        public static void numberFormatException() {
+            // Напишите свое решение ниже
+           
+     }
+    }
+}
---
+++ Handling Errors in Programming/sem1/hw2.java
@@ -0,0 +1,9 @@
+/*
+ * 
+ */
+
+public class hw2 {
+    public static void main(String[] args) {
+        
+    }
+}
