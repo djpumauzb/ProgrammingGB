10c392540a3ada7cbb97fafd9e9a7de9ed696294
Sirojiddin <sirojboy@inbox.ru>
2024-03-17 06:14:17 +0500

    updt smthng

--- Handling Errors in Programming/sem3/Main.java
+++ Handling Errors in Programming/sem3/Main.java
@@ -6,14 +6,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите данные в формате: Фамилия Имя Отчество дата_рождения номер_телефона пол");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Введите данные в формате: Фамилия Имя Отчество дата_рождения номер_телефона пол");
 
-        String input = scanner.nextLine();
-        try {
-            processInput(input);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            String input = scanner.nextLine();
+            try {
+                processInput(input);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 
